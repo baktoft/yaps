e820d9ca6b31b8b86b8bd1cf0a4ddc9f9101c9d1
Henrik Baktoft <hba@aqua.dtu.dk>
2020-02-17 16:09:15 +0100

    add optional xlim, ylim and main in plotYaps()

--- R/plotYaps.R
+++ R/plotYaps.R
@@ -3,8 +3,10 @@
 #' @param inp Input object obtained using \code{getInp()}
 #' @param yaps_out Output from succesful run of \code{runYaps()}
 #' @param type Plot type. \code{type="map"} prodces a basic map of estimated track and hydrophones; \code{type="coord_X"}, \code{type="coord_Y"} produces plots of X and Y coordinated including +- 1 standard error.
+#' @param xlim,ylim Optional vectors of length 2 to set xlim and/or ylim. 
+#' @param main Title of plot - optional.
 #' @export
-plotYaps <- function(inp, yaps_out, type="map"){
+plotYaps <- function(inp, yaps_out, type="map", xlim=NULL, ylim=NULL, main=NULL){
 	pl <- yaps_out$pl
 	plsd <- yaps_out$plsd
 
@@ -14,11 +16,23 @@ plotYaps <- function(inp, yaps_out, type="map"){
 	
 	hydros <- data.frame(hx=inp$datTmb$H[,1] + inp$inp_params$Hx0, hy=inp$datTmb$H[,2] + inp$inp_params$Hy0)
 	
-	xlim <- range(c(pl$X, hydros$hx))
-	ylim <- range(c(pl$Y, hydros$hy))
+	if(is.null(xlim)){
+		xlim <- range(c(pl$X, hydros$hx))
+	} else {
+		xlim <- xlim
+	}
+	if(is.null(ylim)){
+		ylim <- range(c(pl$Y, hydros$hy))
+	} else {
+		ylim <- ylim
+	}
+	
+	if(is.null(main)){
+		main <- ""
+	}
 	
 	if(type=="map"){
-		plot(hy~hx, data=hydros, col="green", pch=20, cex=2, asp=1, xlab="UTM_X", ylab="UTM_Y", xlim=xlim, ylim=ylim)
+		plot(hy~hx, data=hydros, col="green", pch=20, cex=2, asp=1, xlab="UTM_X", ylab="UTM_Y", xlim=xlim, ylim=ylim, main=main)
 		lines(pl$Y~pl$X, col="red")
 	} else if(type=="coord_X"){
 		plot(pl$top, pl$X, type="l", col="red", ylab="UTM_X", xlab="TimeOfPing")

72e78e6b3608e8318c65388a52de5c92898709f9
Henrik Baktoft <hba@aqua.dtu.dk>
2024-11-19 12:34:41 +0100

    add check for nrow(unique(dat_sync)) != nrow(dat_sync) in checkInpSync

--- R/sync_checkInpSyncData.R
+++ R/sync_checkInpSyncData.R
@@ -7,6 +7,15 @@ checkInpSyncData <- function(hydros, dat_sync, dat_ss, sync_params){
 	
 	checkHydros(hydros)
 	
+	if(!is.na(sync_params$lin_corr[1])){
+		if(!identical(as.integer(rownames(sync_params$lin_corr)),  hydros$h_sn)){
+			Es <- Es + 1
+			cat("	ERROR: Inconsistency in hydro serials specified in lin_corr vs hydros. 
+					... lin_corr was probably obtained based on a different data set. \n")
+			stopSilent()
+		}
+	}
+	
 	if(nrow(hydros[sync_params$time_keeper %in% h_sn]) == 0){
 		Es <- Es + 1
 		cat("ERROR: The specified sync_params$time_keeper is not present in hydros. \n")
@@ -33,14 +42,19 @@ checkInpSyncData <- function(hydros, dat_sync, dat_ss, sync_params){
 		det_hydros <- unique(dat_sync$h_sn)
 		not_dets <- hydros$h_sn[!hydros$h_sn %in% det_hydros]
 		stop(paste0("ERROR: These hydro serials were not found in the detection tabel. They cannot be synced. Please fix or remove. \n",paste(not_dets, collapse=", "),"\n"))
-		Ws <- Ws + 1
+		Es <- Es + 1
 	}
 	
 	if(sum(!unique(dat_sync$h_sn) %in% hydros$h_sn) != 0){
 		det_hydros <- unique(dat_sync$h_sn)
 		not_dets <- det_hydros[!hydros$h_sn %in% det_hydros]
 		stop(paste0("ERROR: These hydro serials are present in the detection tabel, but not in hydros. Please fix or remove. \n",paste(not_dets, collapse=", "),"\n"))
-		Ws <- Ws + 1
+		Es <- Es + 1
+	}
+	
+	if(nrow(unique(dat_sync)) != nrow(dat_sync)){
+		cat("ERROR: Duplicate rows found in dat_sync. Might cause problems when building TOA-matrix. Maybe need to filter for protocol and/or multipath?\n")
+		Es <- Es + 1
 	}
 	
 	
